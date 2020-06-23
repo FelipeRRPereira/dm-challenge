d512da3d417977b25ad8eb13a59a445f19dc35d0
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2020-06-22 23:33:04 -0300

    Adicionado endpoint recipe puppy no .env.

--- config/config.default.js
+++ config/config.default.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 
-const { PORT = "3333", HOST = "0.0.0.0" } = process.env;
+const {
+  PORT = "3333",
+  HOST = "0.0.0.0",
+  HOST_RECIPE_PUPPY = "http://www.recipepuppy.com/api/",
+} = process.env;
 
 const config = {
   port: PORT,
   host: HOST,
+  hostRecipePuppy: HOST_RECIPE_PUPPY,
 };
 
 module.exports = { config };
