2b7d7771f876ff8fc3a84fdfbed010832d4f6ff8
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2020-06-23 04:03:04 -0300

    Retirado aspas da porta e key.

--- config/config.default.js
+++ config/config.default.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 const {
-  PORT = "3333",
+  PORT = 3333,
   HOST = "0.0.0.0",
   HOST_RECIPE_PUPPY = "http://www.recipepuppy.com/api/",
   HOST_GIPHY = "https://api.giphy.com/v1/gifs/search",
-  API_KEY_GIPHY = "Ai07SVKbuScrxS8wp3UDKj0E1K4ocfaC",
+  API_KEY_GIPHY,
 } = process.env;
 
 const config = {
