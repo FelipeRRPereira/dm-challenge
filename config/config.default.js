dbed2c056e1d68474944ff75b3e0370445fd4a50
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2020-06-23 03:46:00 -0300

    Variaveis de ambiente para URL e API_KEY do Giphy.

--- config/config.default.js
+++ config/config.default.js
@@ -4,12 +4,16 @@ const {
   PORT = "3333",
   HOST = "0.0.0.0",
   HOST_RECIPE_PUPPY = "http://www.recipepuppy.com/api/",
+  HOST_GIPHY = "https://api.giphy.com/v1/gifs/search",
+  API_KEY_GIPHY = "Ai07SVKbuScrxS8wp3UDKj0E1K4ocfaC",
 } = process.env;
 
 const config = {
   port: PORT,
   host: HOST,
   hostRecipePuppy: HOST_RECIPE_PUPPY,
+  hostGiphy: HOST_GIPHY,
+  apiKeyGiphy: API_KEY_GIPHY,
 };
 
 module.exports = { config };
